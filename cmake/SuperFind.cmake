ba1fb7c34d90df4b9a1c889c6f57ddd9ae1a4727
Rashad <mohammedrashadkm@gmail.com>
2015-12-28 21:34:26 +0100

    pass generator and other stuff when configure the other one

--- cmake/SuperFind.cmake
+++ cmake/SuperFind.cmake
@@ -33,24 +33,23 @@ function(super_find_package name)
       DOWNLOAD_COMMAND ""
       CONFIGURE_COMMAND ""
       UPDATE_COMMAND ""        
-      INSTALL_COMMAND
-      )
-    
+      INSTALL_COMMAND)
     set(${PKG_NAME}_INSTALL_DIR ${ep_base}/Install/${PKG_NAME})
     
+    #download source code.
     if(NOT EXISTS "${ep_base}/Stamp/${PKG_NAME}/${PKG_NAME}-download")
-
       execute_process(COMMAND git clone ${EP_URL}/${PKG_REPO} ${PKG_NAME}
         WORKING_DIRECTORY  ${ep_base}/Source)
-      
       execute_process(COMMAND ${CMAKE_COMMAND}  -E touch "${PKG_NAME}-download"
         WORKING_DIRECTORY ${ep_base}/Stamp/${PKG_NAME} )  
     endif()
-    
+    #configure
     execute_process(COMMAND ${CMAKE_COMMAND} ${ep_base}/Source/${PKG_NAME}
       "-DCMAKE_INSTALL_PREFIX=${${PKG_NAME}_INSTALL_DIR}"
+      "-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
+      "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+      "-G${CMAKE_GENERATOR}"
       WORKING_DIRECTORY ${ep_base}/Build/${PKG_NAME} )
-
     include(${ep_base}/Build/${PKG_NAME}/${PKG_NAME}Config.cmake)
     
   endif()

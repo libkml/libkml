835bab700bf3b6fb9b1d41ec635993fbce4926f5
Rashad Kanavath <rashadkm@gmail.com>
2016-03-03 19:29:43 +0530

    add cmake find files for cmake < 3.4

---
+++ cmake/FindMiniZip.cmake
@@ -0,0 +1,35 @@
+# Find the Minizip library
+# Defines:
+
+#  MINIZIP_INCLUDE_DIR - minizip include directory
+#  MINIZIP_LIBRARY     - minizip library file
+#  MINIZIP_FOUND       - TRUE if minizip is found
+
+if (MINIZIP_INCLUDE_DIR)
+ #check cache 
+  set(MINIZIP_FIND_QUIETLY TRUE)
+endif ()
+
+if (NOT MINIZIP_INCLUDE_DIR)
+    find_path(MINIZIP_INCLUDE_DIR NAMES unzip.h PATH_SUFFIXES minizip)
+    set(MINIZIP_INCLUDE_DIR ${MINIZIP_INCLUDE_DIR}/minizip CACHE PATH "minizip includes")
+endif ()
+
+find_library(MINIZIP_LIBRARY NAMES minizip)
+
+if (MINIZIP_INCLUDE_DIR AND MINIZIP_LIBRARY)
+  set(MINIZIP_FOUND TRUE)
+  set(MiniZip_FOUND TRUE)
+endif ()
+
+if (MINIZIP_FOUND)
+   if (NOT MINIZIP_FIND_QUIETLY)
+      message(STATUS "Found Minizip library: ${MINIZIP_LIBRARY}")
+   endif ()
+else ()
+   if (NOT MINIZIP_FIND_QUIETLY)
+      message(FATAL_ERROR "Could NOT find Minizip library")
+    else ()
+      message(STATUS "Could NOT find Minizip library")
+    endif ()
+endif ()
